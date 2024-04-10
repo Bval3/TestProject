a82b6d5c695fa7ce5868cf75073788b07befbd79
Ben <vben31905@gmail.com>
2024-04-10 13:44:23 -0400

    initial commit

---
+++ gitDemo.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author Benjamin Valenzuela
+ */
+public class gitDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Dr. Johnson! This is Benjamin Valenzuela.");
+
+	}
+}
